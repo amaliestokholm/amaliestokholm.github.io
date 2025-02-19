7f6d52946c6796e00ca36ac84e1e4e461c8d6cca
Amalie <amaliestokholm@gmail.com>
2025-02-19 12:51:20 +0000

    Add carousel

---
+++ _site/js/landing-page.js
@@ -0,0 +1,201 @@
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+	$('a.page-scroll').bind('click', function(event) {
+		var $anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $($anchor.attr('href')).offset().top
+		}, 1500, 'easeInOutExpo');
+		event.preventDefault();
+	});
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+	$('.navbar-toggle:visible').click();
+});
+
+$('div.modal').on('show.bs.modal', function() {
+	var modal = this;
+	var hash = modal.id;
+	window.location.hash = hash;
+	window.onhashchange = function() {
+		if (!location.hash){
+			$(modal).modal('hide');
+		}
+	}
+});
+particlesJS("particles-js", {
+	"particles": {
+		"number": {
+			"value": 160,
+			"density": {
+				"enable": true,
+				"value_area": 800
+			}
+		},
+		"color": {
+			"value": "#ffffff"
+		},
+		"shape": {
+			"type": "circle",
+			"stroke": {
+				"width": 0,
+				"color": "#000000"
+			},
+			"polygon": {
+				"nb_sides": 5
+			},
+			"image": {
+				"src": "img/github.svg",
+				"width": 100,
+				"height": 100
+			}
+		},
+		"opacity": {
+			"value": 1,
+			"random": true,
+			"anim": {
+				"enable": true,
+				"speed": 1,
+				"opacity_min": 0,
+				"sync": false
+			}
+		},
+		"size": {
+			"value": 3,
+			"random": true,
+			"anim": {
+				"enable": false,
+				"speed": 4,
+				"size_min": 0.3,
+				"sync": false
+			}
+		},
+		"line_linked": {
+			"enable": false,
+			"distance": 150,
+			"color": "#ffffff",
+			"opacity": 0.4,
+			"width": 1
+		},
+		"move": {
+			"enable": true,
+			"speed": 8,
+			"direction": "none",
+			"random": true,
+			"straight": false,
+			"out_mode": "out",
+			"bounce": false,
+			"attract": {
+				"enable": false,
+				"rotateX": 600,
+				"rotateY": 1200
+			}
+		}
+	},
+	"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+			"onhover": {
+				"enable": true,
+				"mode": "bubble"
+			},
+			"onclick": {
+				"enable": true,
+				"mode": "push"
+			},
+			"resize": true
+		},
+		"modes": {
+			"grab": {
+				"distance": 400,
+				"line_linked": {
+					"opacity": 1
+				}
+			},
+			"bubble": {
+				"distance": 83.91608391608392,
+				"size": 1,
+				"duration": 3,
+				"opacity": 1,
+				"speed": 3
+			},
+			"repulse": {
+				"distance": 200,
+				"duration": 0.4
+			},
+			"push": {
+				"particles_nb": 4
+			},
+			"remove": {
+				"particles_nb": 2
+			}
+		}
+	},
+	"retina_detect": true
+});
+$("#particles-header").insertAfter(".particles-js-canvas-el");
+
+// From http://www.ifa.hawaii.edu/~taberger
+function enableCounterUp(a){
+	
+	var counterElement;
+	
+	if(isExists('#counter')){ counterElement = $('#counter'); }
+	else{ return; }
+		
+	var oTop = $('#counter').offset().top - window.innerHeight;
+	if (a == 0 && $(window).scrollTop() > oTop) {
+		$('.counter-value').each(function() {
+			var $this = $(this),
+				countDuration = $this.data('duration'),
+				countTo = $this.attr('data-count');
+			$({
+				countNum: $this.text()
+			}).animate({
+				countNum: countTo
+			},{
+
+				duration: countDuration,
+				easing: 'swing',
+				step: function() {
+					$this.text(Math.floor(this.countNum));
+				},
+				complete: function() {
+					$this.text(this.countNum);
+				}
+
+			});
+		});
+		a = 1;
+	}
+
+	return a;
+}
+
+function isExists(elem){
+	if ($(elem).length > 0) { 
+		return true;
+	}
+	return false;
+}
+
+(function ($) {
+
+	"use strict";
+
+	var countCounterUp = 0;
+
+	var a = 0 ;
+
+	countCounterUp = enableCounterUp(countCounterUp);
+
+	$(window).on('scroll', function(){
+
+		countCounterUp = enableCounterUp(countCounterUp);
+
+	});
+
+
+})(jQuery);
+
